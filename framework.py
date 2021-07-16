640e6f6e1d58ba5380875d0eafef4ceb8e2ff04d
rahul <rahul003chry@gmail.com>
2021-07-16 11:15:01 +0530

    framework.py

--- framework.py
+++ framework.py
@@ -7,7 +7,7 @@ TODO:
 #####################################################
 #						Import						#
 #####################################################
-import pygame, math, random, sys
+import pygame, math, random, sys, time
 from pygame.locals import *
 
 pygame.init()
@@ -18,7 +18,7 @@ OBJECTS = []
 DEFAULT_KEYS = {
 				"right": {
 						"keystrokes": [K_RIGHT, K_d],
-						"keystrength": -1
+						"keystrength": 1
 					},
 				"left": {
 						"keystrokes": [K_LEFT, K_a],
@@ -26,7 +26,7 @@ DEFAULT_KEYS = {
 					},
 				"down": {
 						"keystrokes": [K_DOWN, K_s],
-						"keystrength": -1
+						"keystrength": 1
 					},
 				"up": {
 						"keystrokes": [K_UP, K_w],
@@ -84,19 +84,32 @@ class Input(object):
 
 		return keyPressed
 
+	def getActionStrength(self, key):
+
+		keys = pygame.key.get_pressed()
+		actionStrength = 0
+		try:
+			for keystroke in self.inputKey[key]["keystrokes"]: 
+				if keys[keystroke]: actionStrength = self.inputKey[key]["keystrength"]
+		except:
+			actionStrength = 1
+
+		return actionStrength
+
+
 #####################################################
 #						Entity						#
 #####################################################
 
 class Entity(object):
 
-	def __init__(self, vec2, width, height, entityType, entityName):
-		self.vec2 = vec2
+	def __init__(self, point, width, height, entityType, entityName):
+		self.point = point
 		self.width = width
 		self.height = height
 		self.entityType = entityType
 		self.entityName = entityName
-		self.physics = PhysicsObject(vec2, width, height) # phy obj
+		self.physics = PhysicsObject(point, width, height) # phy obj
 		self.collisionShape2D = self.physics.rect # collision shape for a obj!
 		self.color = (0, 0, 0) # Black
 		self.sprite = None # obj sprite
@@ -111,26 +124,54 @@ class Entity(object):
 	def draw(self, surface):
 		return pygame.draw.rect(surface, self.color, self.collisionShape2D)
 
+	def __repr__(self):
+		return f"<Entity({self.point}, {self.width}, {self.height}, {self.entityType}, {self.entityName}, {self.physics})>"
+
 #####################################################
 #						Physics						#
 #####################################################
 
 class PhysicsObject(object):
-	def __init__(self, vec2, width, height):
-		self.vec2 = vec2
+	def __init__(self, point, width, height):
+		self.point = point
 		self.width = width
 		self.height = height
-		self.rect = pygame.Rect(self.vec2.x, self.vec2.y, self.width, self.height)
+		self.rect = pygame.Rect(self.point.x, self.point.y, self.width, self.height)
 		self.collision = None
 		self.collideMask = []
+		self.movement = Vector2()
 
 	def move(self, movement):
 		self.collision = Collision(self, movement)
-		return self.collision.FindRectRectCollision(), Vector2(self.vec2.x, self.vec2.y)
+		return self.collision.FindRectRectCollision(), Vector2(self.point.x, self.point.y)
 
 	def isOnFloor(self):
 		if self.collision.bottom: return True
 		return False
+
+	def jump(self, gravity, speed, delta, inputEvent, jumpKey):
+
+		# m.x = (inputEvent.getActionStrength("right") - inputEvent.getActionStrength("left")) * 5
+		# v = Vector2(
+		# 		v.x,
+		# 		-10 * 500 if inputEvent.isKeyPressed("up") and player.physics.isOnFloor() else 10 * 5
+		# 	)
+
+		# m.y = clamp(m.y, 5)
+		# m.y = clamp(m.y, -10)
+
+		# m.y = m.y + delta * v.y
+		# v.y = v.y + delta * g.y
+
+		velocity = speed["up"] if inputEvent.isKeyPressed(jumpKey) and self.isOnFloor() else speed["down"]
+		self.movement.y = clamp(self.movement.y, speed["down"] / 100 * 10)
+		self.movement.y = clamp(self.movement.y, speed["up"] / 100 * 30)
+
+		# main formula
+		self.movement.y = self.movement.y + delta * velocity
+		velocity = velocity + delta * gravity
+
+		return self.movement
 
 class Collision(object):
 	
@@ -141,7 +182,7 @@ class Collision(object):
 
 	def __init__(self, physicsObject, movement):
 		self.physicsObject = physicsObject
-		self.movement = movement # Vec2
+		self.movement = movement # point
 		self.collisionLevel = 50
 
 	def FindRectRectCollision(self):
@@ -155,11 +196,14 @@ class Collision(object):
 				if object["rect"].colliderect(self.physicsObject.rect) and object["type"] == "static": collision.append(object)
 
 
-		self.physicsObject.vec2.x += self.movement.x
+		self.physicsObject.point.x += self.movement.x
 		self.physicsObject.rect.x += self.movement.x
 
 		for block in collision:
 			# print(abs(block["rect"].top - self.physicsObject.rect.bottom))
+
+			# if block["rect"].width < self.collisionLevel: self.collisionLevel = block["rect"].width * 2
+
 			if abs(block["rect"].left - self.physicsObject.rect.right) < self.collisionLevel and self.movement.x > 0:
 				self.physicsObject.rect.right = block["rect"].left + 1
 				self.right = True
@@ -168,11 +212,14 @@ class Collision(object):
 				self.physicsObject.rect.left = block["rect"].right - 1
 				self.left = True
 
-		self.physicsObject.vec2.y += self.movement.y
+		self.physicsObject.point.y += self.movement.y
 		self.physicsObject.rect.y += self.movement.y
 
 		for block in collision:
 			# print(abs(block["rect"].top - self.physicsObject.rect.bottom))
+
+			# if block["rect"].width < self.collisionLevel: self.collisionLevel = block["rect"].width * 2
+
 			if abs(block["rect"].top - self.physicsObject.rect.bottom) < self.collisionLevel and self.movement.y > 0:
 				self.physicsObject.rect.bottom = block["rect"].top + 1
 				self.bottom = True
@@ -198,23 +245,45 @@ class Vector2(object):
 	def ZERO(self):
 		return Vector2(0, 0)
 
-	def clamp(self, a, b):
-
-		if a > b: return b
-		return a
-
 	def __repr__(self):
 		return f"<Vector2({self.x, self.y})>"
 
 	def __add__(self, other):
+		if isinstance(other, float): return Vector2(self.x + other, self.y + other)
 		return Vector2(self.x + other.x, self.y + other.y)
 
 	def __mul__(self, other):
-		# if isinstance(other, float): return Vector2(self.x * other, self.y * other)
+		if isinstance(other, float) or isinstance(other, int): return Vector2(self.x * other, self.y * other)
 		return Vector2(self.x * other.x , self.y * other.y)
+
+class Point(object):
+
+	def __init__(self, x=0, y=0):
+		self.x = x
+		self.y = y
+
+	def __repr__(self):
+		return f"<Point({self.x, self.y})>"
+
+	def __add__(self, other):
+		if isinstance(other, float): return Vector2(self.x + other, self.y + other)
+		return Point(self.x + other.x, self.y + other.y)
+
+	def __mul__(self, other):
+		if isinstance(other, float): return Vector2(self.x * other, self.y * other)
+		return Point(self.x * other.x , self.y * other.y)
 
 def fpsLock(fps):
 	return clock.tick(fps)
+
+def clamp(a, b):
+
+	if b > 0:
+		if a > b: return b
+	elif b < 0:
+		if a < b: return b
+	return a
+
 
 #####################################################
 #						Test						#
@@ -222,27 +291,31 @@ def fpsLock(fps):
 
 def main():
 	window = Window(800, 600, "Test Framework")
-	player = Entity(Vector2(100, 100), 100, 200, "dynamic", "player")
-	floor = Entity(Vector2(50, 500), 600, 50, "static", "player")
-
+	player = Entity(Point(100, 100), 100, 200, "dynamic", "player")
+	floor = Entity(Point(0, 600), 1300, 100, "static", "world")
+	block = Entity(Point(600, 550), 30, 30, "static", "world")
 	inputEvent = Input(DEFAULT_KEYS)
-	movement = Vector2()
 
-	delta = fpsLock(60) / 1000
+	def playerDraw(delta):
+		m = Vector2(
+			(inputEvent.getActionStrength("right") - inputEvent.getActionStrength("left")) * 10,
+			player.physics.jump(3, {"down": 100, "up": -3000}, delta, inputEvent, "up").y
+		)
 
-	def playerDraw():
-		movement.y += 10 * delta
-		movement.y = movement.clamp(movement.y, 2)
-
+		player.physics.move(m)
 		player.draw(window.screen)
-		player.physics.move(movement)
 
 	while True:
 		window.screen.fill((255, 255, 255))
-		playerDraw()
+		playerDraw(fpsLock(120) / 1000)
 		floor.draw(window.screen)
+		block.draw(window.screen)
+
+		rect = pygame.Rect(10, 10, 10, 10)
+		# print(rect.height)
+
 		window.update()
-		fpsLock(60)
+		fpsLock(120)
 
 
 if __name__ == '__main__':
